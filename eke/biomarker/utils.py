88af2b57930a89fa97e02022dbbca177a87e5e35
Sean Kelly <sean.c.kelly@me.com>
2011-06-14 03:12:57 +0000

    Implement mapping form BMDB names to CollabGroup names

---
+++ eke/biomarker/utils.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+# Copyright 2011 California Institute of Technology. ALL RIGHTS
+# RESERVED. U.S. Government Sponsorship acknowledged.
+
+COLLABORATIVE_GROUP_BMDB_IDS_TO_NAMES = {
+    u'Breast and Gynecologic':          u'Breast and Gynecologic Cancers Research Group',
+    u'G.I. and Other Associated':       u'G.I. and Other Associated Cancers Research Group',
+    u'Lung and Upper Aerodigestive':    u'Lung and Upper Aerodigestive Cancers Research Group',
+    u'Prostate and Urologic':           u'Prostate and Urologic Cancers Research Group',
+}
