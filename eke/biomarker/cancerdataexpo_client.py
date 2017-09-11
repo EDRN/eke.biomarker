2a2d3275813d6efa8a0a60ba45cd9c33c18c4f61
yuliujpl <david.liu@jpl.nasa.gov>
2017-09-11 14:53:40 -0700

    Removed ugly print statements, will use logger next time.

--- eke/biomarker/cancerdataexpo_client.py
+++ eke/biomarker/cancerdataexpo_client.py
@@ -5,23 +5,16 @@ def getBiomarkerLinks(biomarker, idDataSource):
         return None
     if idDataSource.strip() == "":
         return None
-    print "START"
     try:    
-        print "GOT HERE"
-        print "request: {}".format(idDataSource+"/"+biomarker)
         r = requests.get(idDataSource+"/"+biomarker, headers={'Accept': 'application/json'})
     except requests.exceptions.ConnectionError:
-        print "GOT CONNECITION ERROR"
         return None
 
     j= r.text
-    print j
     jsonresults = None
     if j != "":
         try:
             jsonresults = ast.literal_eval(j)
-            print "JSON Successful!!!"
         except:
-            print "JSON FAILED, WARNING!"
             pass
     return jsonresults
